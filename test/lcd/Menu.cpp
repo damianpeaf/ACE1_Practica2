97610b598a38206fc926834a9039ec289167b3bb
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-02-17 18:04:23 -0600

    feat: icons to use with lcd

--- test/lcd/Menu.cpp
+++ test/lcd/Menu.cpp
@@ -1,1 +1,42 @@
+#include <LiquidCrystal.h>
 #include "Menu.h"
+
+
+LiquidCrystal lcd(2,3,4,5,6,7); // RS, E, D4, D5, D6, D7
+
+clockChar[8] = {
+  
+  B11111,
+  B01010,
+  B01010,
+  B00100,
+  B01010,
+  B01010,
+  B11111,
+  B00000,
+};
+
+readyChar[8] = {
+  B00000,
+  B11011,
+  B01010,
+  B10001,
+  B10001,
+  B01110,
+  B00000,
+  B00000
+};
+
+void setup() {
+  lcd.createChar(0,clockChar);
+  lcd.begin(16,2);
+  lcd.write(byte(0));
+  lcd.print("GPR 07 B ACE1");
+  lcd.write(byte(0));
+  lcd.setCursor(0,1);
+  lcd.print("starting");
+
+}
+
+void loop() { 
+}
--- test/lcd/Menu.h
+++ test/lcd/Menu.h
@@ -3,6 +3,8 @@
 
 extern String initialMessage;
 extern String readyMessage;
+extern byte[8] clockChar;
+extern byte[8] readyChar;
 
 void menu_init();
 void menu_print(String text);
