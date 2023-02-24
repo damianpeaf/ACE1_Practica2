d38d5197ef7a264d7b576554afac800026ed16b1
FIUSAC_AlvaroGarcía <97930556+AlvaroG13191704@users.noreply.github.com>
2023-02-24 12:51:27 -0600

    i2c te

--- test/I2C_TEST/I2C_TEST.ino
+++ test/I2C_TEST/I2C_TEST.ino
@@ -1,23 +1,35 @@
-#include "Wire.h" 
-//MAESTRO
-byte CODE;
-byte respuesta;
-void setup()
-{
-  Serial.begin(9600);
-  // maestro
-  Wire.begin();
-   // preguntar al esclavo
+#include <Wire.h> 
+byte CODE; // This is the point to traslated messages
 
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(); // Start the communication, here is the master
+  Serial.begin(9600);
+  Serial.println("From Master, and the I2C was set");
+  sendDataToSlave();
 }
 
-void loop()
-{
-  
-  Wire.requestFrom(0x01,1);
-  // leer lo que entra del esclavo
-  while(Wire.available()){
-    CODE = Wire.read();
+void sendDataToSlave() {
+  int value = 20;
+  Wire.beginTransmission(0x01);
+  Wire.write(value);
+  Wire.endTransmission();
+  delay(1000);
+  Serial.println("Escribiendo al slave");
+
+}
+void receiveData(){
+  // Request data from the slave 
+  Wire.requestFrom(0x01,1); // From the slave address, only 1 data transfer, this may vary
+  while(Wire.available()){ // While both arduino's are connected...
+    CODE = Wire.read(); // read the code that is being transferred
   }
-  Serial.println(CODE);
+  Serial.println(CODE);  
+}
+
+
+void loop() {  
+  
+  // put your main code here, to run repeatedly:
+  receiveData();
 }
