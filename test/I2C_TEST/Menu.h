04bd2582b87090281607e2b20e47d4f6ca881e9f
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-02-26 18:03:36 -0600

    fix: variables in menu h and menu cpp

--- test/I2C_TEST/Menu.h
+++ test/I2C_TEST/Menu.h
@@ -8,13 +8,13 @@ extern String readyMessage;
 extern int actualTime;
 extern byte clockChar[8];
 extern byte readyChar[8];
-const int buttonPin1; // Pin del botón para avanzar
-const int buttonPin2;  // Pin del botón para retroceder
-int buttonState1;     // Estado del botón para avanzar
-int buttonState2;     // Estado del botón para retroceder
-int lastButtonState1; // Último estado del botón para avanzar
-int lastButtonState2; // Último estado del botón para retroceder
-int messageNum;       // Número de mensaje actual
+extern const int buttonPin1; // Pin del botón para avanzar
+extern const int buttonPin2;  // Pin del botón para retroceder
+extern int buttonState1;     // Estado del botón para avanzar
+extern int buttonState2;     // Estado del botón para retroceder
+extern int lastButtonState1; // Último estado del botón para avanzar
+extern int lastButtonState2; // Último estado del botón para retroceder
+extern int messageNum;       // Número de mensaje actual
 
 void initial_menu();
 void ready_menu();
